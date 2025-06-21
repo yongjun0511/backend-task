f096dd5836dad0584003517ccc52c853320e65a7
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-06-21 18:12:27 +0900

    refactor (main): dto변경 사항 점검 완료 및 README 중간 업데이트

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,46 @@
 package main
 
 import (
+	"banksalad-backend-task/internal/domain"
 	"banksalad-backend-task/internal/handler/preprocess"
 	"log"
+	"os"
 
 	"banksalad-backend-task/internal/handler/notifier"
 	"banksalad-backend-task/internal/handler/notifier/channelhandler"
 	"banksalad-backend-task/internal/handler/preprocess/parser"
 	"banksalad-backend-task/internal/handler/preprocess/validator"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.WithField("reason", r).Error("💥 application initialization failed")
+			os.Exit(1)
+		}
+	}()
+
 	pp := preprocess.NewPreprocessor(
 		"files/input/data.txt",
 		parser.NewDefaultParser(),
 		validator.NewDefaultValidator(),
 	)
 
-	result, err := pp.Run()
-	if err != nil {
-		log.Fatalf("[ERROR] Preprocessing failed: %v", err)
-	}
-
 	emailHandler := channelhandler.NewEmailHandler()
 	smsHandler := channelhandler.NewSMSHandler()
 
-	nt := notifier.NewNotifier([]channelhandler.ChannelHandler{
-		emailHandler,
-		smsHandler,
+	nt := notifier.NewNotifier(map[domain.FieldType]channelhandler.ChannelHandler{
+		domain.EmailField: emailHandler,
+		domain.PhoneField: smsHandler,
 	})
+
+	result, err := pp.Run()
+	if err != nil {
+		logrus.WithError(err).Fatal("preprocessing failed")
+	}
 
 	if err := nt.NotifyAll(result); err != nil {
 		log.Fatalf("[ERROR] Notification failed: %v", err)
