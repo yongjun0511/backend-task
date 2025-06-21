1221e5061bbb87df5f2cc26f6fede92ff36cb258
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-06-21 20:35:23 +0900

    refactor (channelhandler): email_handler.go 에러 관련 리펙토링

--- internal/handler/notifier/channelhandler/email_handler.go
+++ internal/handler/notifier/channelhandler/email_handler.go
@@ -1,13 +1,16 @@
 package channelhandler
 
 import (
-	"fmt"
-	"log"
+	"github.com/pkg/errors"
 	"sync"
 
 	"banksalad-backend-task/clients"
 	"banksalad-backend-task/internal/domain"
+
+	"github.com/sirupsen/logrus"
 )
+
+const maxRetry = 3
 
 type EmailHandler struct {
 	client *clients.EmailClient
@@ -27,7 +30,7 @@ func (h *EmailHandler) TargetField() domain.FieldType {
 func (h *EmailHandler) SendBatch(values []string) error {
 	for _, email := range values {
 		var err error
-		for i := 0; i < 3; i++ {
+		for i := 0; i < maxRetry; i++ {
 			h.mu.Lock()
 			err = h.client.Send(email, "신용 점수가 상승했습니다!")
 			h.mu.Unlock()
@@ -35,11 +38,16 @@ func (h *EmailHandler) SendBatch(values []string) error {
 			if err == nil {
 				break
 			}
-			log.Printf("[WARN] 이메일 전송 실패 (%d/3): %s, err: %v", i+1, email, err)
+			logrus.WithFields(logrus.Fields{
+				"attempt": i + 1,
+				"max":     maxRetry,
+				"email":   email,
+				"error":   err,
+			}).Warn("email send failed")
 		}
 
 		if err != nil {
-			return fmt.Errorf("이메일 전송 실패 (email: %s): %w", email, err)
+			return errors.Wrapf(err, "email send failed after %d attempts (%s)", maxRetry, email)
 		}
 	}
 	return nil
